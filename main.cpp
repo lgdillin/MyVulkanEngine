1b91e5c48897abcbe47b270b984d5e45a168e57e
Liam Dillingham <mailgd9@gmail.com>
2024-01-26 12:53:29 -0600

    again

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-#include <vulkan/vulkan.h>
+//#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
@@ -7,6 +9,7 @@
 class HelloTriangleApplication {
 public:
 	void run() {
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanUp();
